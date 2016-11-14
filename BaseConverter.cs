8acb9172f1286221fa67911ac88594f80cb35382
Sebastian Gruchacz <sebastian@gruchacz.com>
2016-11-14 11:56:08 +0100

    Add files via upload

---
+++ BaseConverter.cs
@@ -0,0 +1,32 @@
+﻿namespace ConverterDistill
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public abstract class BaseConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty FormatterProperty = 
+            DependencyProperty.Register("Formatter", typeof(string), typeof(BaseConverter), new PropertyMetadata(default(string)));
+
+        public string Formatter
+        {
+            get
+            {
+                return (string)GetValue(FormatterProperty);
+            }
+            set
+            {
+                SetValue(FormatterProperty, value);
+            }
+        }
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value; // Just for sake
+        }
+    }
+}
